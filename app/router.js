31e800cca0db3c3ac0d59b859ab3dfac172bf97f
Fabio Mazzone <fabio.mazzone@me.com>
2016-04-26 21:40:30 +0200

    generated some pages but not all

--- app/router.js
+++ app/router.js
@@ -6,6 +6,10 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('home');
+  this.route('profile');
+  this.route('achievments');
+  this.route('leaderboard');
 });
 
 export default Router;
