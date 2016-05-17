d41c93ed3e8f5286634f3cafeb3769886b24b4ed
soeren van der wall <soeren.vanderwall@web.de>
2016-05-17 15:03:51 +0200

    next level shit datapoooooooooooorn

--- app/components/profile-chart.js
+++ app/components/profile-chart.js
@@ -1,47 +1,48 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
- tagName: 'canvas',
+  tagName: 'canvas',
 
-   didRender() {
-     console.log('asdasd');
-     var ctx = this.$();
-     console.log(ctx);
-     new Chart(ctx, {
-       type: 'radar',
-       data : {
-                  labels: ["Eating", "Drinking", "Sleeping", "Designing", "Coding", "Cycling", "Running"],
-                  datasets: [
-                      {
-                          label: "My First dataset",
-                          backgroundColor: "rgba(179,181,198,0.2)",
-                          borderColor: "rgba(179,181,198,1)",
-                          pointBackgroundColor: "rgba(179,181,198,1)",
-                          pointBorderColor: "#fff",
-                          pointHoverBackgroundColor: "#fff",
-                          pointHoverBorderColor: "rgba(179,181,198,1)",
-                          data: [65, 59, 90, 81, 56, 55, 40]
-                      },
-                      {
-                          label: "My Second dataset",
-                          backgroundColor: "rgba(255,99,132,0.2)",
-                          borderColor: "rgba(255,99,132,1)",
-                          pointBackgroundColor: "rgba(255,99,132,1)",
-                          pointBorderColor: "#fff",
-                          pointHoverBackgroundColor: "#fff",
-                          pointHoverBorderColor: "rgba(255,99,132,1)",
-                          data: [28, 48, 40, 19, 96, 27, 100]
-                      }
-                  ]
-              },
-       options : {
-                             scale: {
-                                 reverse: true,
-                                 ticks: {
-                                     beginAtZero: true
-                                 }
-                             }
-                     }
-     });
-   }
+    didRender() {
+      Chart.defaults.global.legend.position = "bottom";
+
+      var ctx = this.$()[0];
+      ctx.height = ctx.width;
+      console.log(ctx);
+      new Chart(ctx, {
+        type: 'radar',
+        data : {
+        labels: ["RA", "SQL", "TRC"],
+        datasets: [
+          {
+            label: "You",
+            backgroundColor: "rgba(179,181,198,0.2)",
+            borderColor: "rgba(179,181,198,1)",
+            pointBackgroundColor: "rgba(179,181,198,1)",
+            pointBorderColor: "#fff",
+            pointHoverBackgroundColor: "#fff",
+            pointHoverBorderColor: "rgba(179,181,198,1)",
+            data: [65, 59, 90]
+          },
+          {
+            label: "ProfileOwner",
+            backgroundColor: "rgba(255,99,132,0.2)",
+            borderColor: "rgba(255,99,132,1)",
+            pointBackgroundColor: "rgba(255,99,132,1)",
+            pointBorderColor: "#fff",
+            pointHoverBackgroundColor: "#fff",
+            pointHoverBorderColor: "rgba(255,99,132,1)",
+            data: [28, 48, 40]
+          }
+        ]
+      },
+      options : {
+        scale: {
+          ticks: {
+            display: false
+          }
+        }
+      }
+    });
+  }
 });
