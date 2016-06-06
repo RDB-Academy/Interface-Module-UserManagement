46181ec888ee612a07d9f2d1392888a1422dbae2
soeren van der wall <soeren.vanderwall@web.de>
2016-06-06 18:44:34 +0200

    wrote test for helper

--- tests/unit/helpers/index-align-features-test.js
+++ tests/unit/helpers/index-align-features-test.js
@@ -5,6 +5,7 @@ module('Unit | Helper | index align features');
 
 // Replace this with your real tests.
 test('it works', function(assert) {
-  let result = indexAlignFeatures([42]);
-  assert.ok(result);
+  let result = indexAlignFeatures(1);
+  let result2 = indexAlignFeatures(2);
+  assert.ok(result && !result2);
 });
