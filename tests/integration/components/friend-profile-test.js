97203d6e26c36e9baa881c2f19ba1c0b6ba3ea41
soeren van der wall <soeren.vanderwall@web.de>
2016-06-06 19:17:50 +0200

    new models for profiles

---
+++ tests/integration/components/friend-profile-test.js
@@ -0,0 +1,24 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('friend-profile', 'Integration | Component | friend profile', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });
+
+  this.render(hbs`{{friend-profile}}`);
+
+  assert.equal(this.$().text().trim(), '');
+
+  // Template block usage:
+  this.render(hbs`
+    {{#friend-profile}}
+      template block text
+    {{/friend-profile}}
+  `);
+
+  assert.equal(this.$().text().trim(), 'template block text');
+});
