cb2c4ae2a11ccf1e17e793ca97943955254db904
soeren van der wall <soeren.vanderwall@web.de>
2016-07-11 21:22:36 +0200

    jshint: routes/friends.js

--- app/routes/friends.js
+++ app/routes/friends.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return {a:this.store.findAll('friend-request'), b:this.store.findAll('friend-profile')}
+    return {a:this.store.findAll('friend-request'), b:this.store.findAll('friend-profile')};
   }
 });
