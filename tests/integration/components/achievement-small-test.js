4e5f8cf89d574ecadeb136aa7d9372f423660855
soeren van der wall <soeren.vanderwall@web.de>
2016-07-11 18:53:46 +0200

    test for achievement-small written

--- tests/integration/components/achievement-small-test.js
+++ tests/integration/components/achievement-small-test.js
@@ -1,5 +1,6 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import Ember from 'ember';
 
 moduleForComponent('achievement-small', 'Integration | Component | achievement small', {
   integration: true
@@ -8,17 +9,21 @@ moduleForComponent('achievement-small', 'Integration | Component | achievement s
 test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
+  let achievement = Ember.Object.create({
+    id: 1,
+    title: "Master of SQL",
+    description: "Do 1000 SQL Statements",
+    image: "img/achievement-logo-01.jpg",
+    achievedAt: "2014-04-25T01:32:21.196Z",
+    valueMin: 0,
+    valueMax: 1000,
+    valueNow: 1000,
+    points: 20
+  });
 
-  this.render(hbs`{{achievement-small}}`);
+  this.set('achievement', achievement);
 
-  assert.equal(this.$().text().trim(), '');
+  this.render(hbs`{{achievement-small achievement}}`);
 
-  // Template block usage:
-  this.render(hbs`
-    {{#achievement-small}}
-      template block text
-    {{/achievement-small}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.ok(this.$()[0].innerHTML.indexOf(this.get('achievement').image) > -1);
 });
