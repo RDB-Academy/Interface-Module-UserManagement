37b3aaf22ae833c1a2339bfc7dbc12bba494f8dd
goldenticket <n.naczk@tu-bs.de>
2016-07-25 15:09:37 +0200

    DonaldTrumpMakesWantToSmokeSomeCrack

--- app/components/home-news-ticker.js
+++ app/components/home-news-ticker.js
@@ -7,27 +7,26 @@ export default Ember.Component.extend({
 
       function setInitialOpacities() {
         var ul = jQuery('#before-tick ul');
-        ul.children('li:nth-child(1)').css("opacity", 0.1);
+        ul.children('li:nth-child(1)').css("opacity", 0);
         ul.children('li:nth-child(2)').css("opacity", 1);
-        ul.children('li:nth-child(3)').css("opacity", 0.1);
+        ul.children('li:nth-child(3)').css("opacity", 0);
       }
 
-      this.$('#before-tick').vTicker({showItems: 3, padding: 0, pause: 5000});
+      this.$('#before-tick').vTicker({showItems: 2, padding: 0, pause: 5000});
       setInitialOpacities();
       this.$('#before-tick').on('vticker.beforeTick', function() {
           var ul = jQuery('#before-tick ul');
           var interval = 500;
-          var colours = ["green", "red", "black", "yellow"];
-          var randomColour = colours[Math.floor(Math.random() * 3)];
+          var colours = ["white"];
 
-          ul.children('li:nth-child(1)').animate({opacity: 0.05}, interval);
-          ul.children('li:nth-child(1)').css("color", randomColour);
-          ul.children('li:nth-child(2)').animate({opacity: 0.1}, interval);
-          ul.children('li:nth-child(2)').css("color", randomColour);
+          ul.children('li:nth-child(1)').animate({opacity: 0}, interval);
+          ul.children('li:nth-child(1)').css("color", colours);
+          ul.children('li:nth-child(2)').animate({opacity: 0}, interval);
+          ul.children('li:nth-child(2)').css("color", colours);
           ul.children('li:nth-child(3)').animate({opacity: 1}, interval);
-          ul.children('li:nth-child(3)').css("color", randomColour);
-          ul.children('li:nth-child(4)').css("opacity", 0.1);
-          ul.children('li:nth-child(4)').css("color", randomColour);
+          ul.children('li:nth-child(3)').css("color", colours);
+          ul.children('li:nth-child(4)').css("opacity", 0);
+          ul.children('li:nth-child(4)').css("color", colours);
       });
 
 
--- app/controllers/leaderboard.js
+++ app/controllers/leaderboard.js
@@ -1,7 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  array: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15],
   state: 'sql',
   isSQLActive: Ember.computed(function() {
     return "btn btn-default" + (this.isActive('sql') ? " active" : "");
