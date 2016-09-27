90b7f9a802621ea2da6bf5a86db68fc2ae319961
gabrielahlers <gabriel.ahlers@gmail.com>
2016-09-27 12:48:30 +0200

    Minor Changes

--- app/controllers/settings/account.js
+++ app/controllers/settings/account.js
@@ -1,24 +1,51 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  ajax: Ember.inject.service(),
+  session: Ember.inject.service('session'),
+
   actions: {
     changePassword() {
-      var oldPassword = this.get('oldPassword');
-      var newPassword1 = this.get('newPassword1');
-      var newPassword2 = this.get('newPassword2');
-      //console.log(oldPassword);
-      //console.log(newPassword1);
-      //console.log(newPassword2);
+      var password = this.get('password');
+      var newPassword = this.get('newPassword');
+      var newPasswordVerification = this.get('newPasswordVerification');
+      console.log(password);
+      console.log(newPassword);
+      console.log(newPasswordVerification);
 
-      if (newPassword1 !== newPassword2) {
-        console.log("Password confirmation incorrect");
-      } else {
-        if (oldPassword === newPassword1) {
-          console.log("Old and new Password are the same");
-          return;
-        }
-        console.log("Insert password -> backend");
+      if (newPassword !== newPasswordVerification) {
+        this.set('newPasswordError', 1);
+        this.set('newPasswordVerificationError', 1);
+        console.log("newPasswordError");
       }
+
+      if (!(Ember.isEmpty(password) || Ember.isEmpty(newPassword) || Ember.isEmpty(newPasswordVerification))) {
+        this.get('ajax').post('/settings/account', {
+          data: JSON.stringify({
+            password: password,
+            newPassword: newPassword
+          })
+        }).then((data) => {
+          console.log(data);
+        }).catch((error) => {
+          console.log(error);
+        });
+      }
+
+
+
+
+
+
+    //  if (newPassword1 !== newPassword2) {
+    //    console.log("Password confirmation incorrect");
+    //  } else {
+    //    if (oldPassword === newPassword1) {
+    //      console.log("Old and new Password are the same");
+    //      return;
+    //    }
+    //    console.log("Insert password -> backend");
+    //  }
 
     }
   }
