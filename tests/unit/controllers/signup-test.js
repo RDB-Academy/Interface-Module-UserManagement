0dc4eac334a3ff54dcba8d66287e7e1c751035fe
goldenticket <n.naczk@tu-bs.de>
2016-09-28 15:50:43 +0200

    some tests, stupid stuff

--- tests/unit/controllers/signup-test.js
+++ tests/unit/controllers/signup-test.js
@@ -10,3 +10,25 @@ test('it exists', function(assert) {
   let controller = this.subject();
   assert.ok(controller);
 });
+
+test('check username validation', function(assert) {
+  var controller = this.subject();
+  //var username = 'jabba@com.de';
+  controller.set('username', 'jabba@com.de');
+  assert.equal(controller.get('usernameError'), -1);
+  controller.set('username', 'fabiomazzone');
+  assert.equal(controller.get('usernameError'), -1);
+  controller.set('username', '@@@@@');
+  assert.equal(controller.get('usernameError'), 1);
+
+});
+
+
+/**
+test('check emailAddress validation', function(assert) {
+  var controller = this.subject();
+  assert.equal(controller.get('emailAddressError'), -1);
+  controller.send('validateEmailAddress');
+  assert.equal(controller.get('emailAddress'), 'jabbcom');
+});
+*/
